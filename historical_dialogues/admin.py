ff23682a43e8fef33f81f8d890001ac9e3ecb297
Cherica <riantsoacherica01@gmail.com>
2025-04-04 19:07:50 +0300

    Auto stash before merge of "cherica" and "origin/Leomyre"

--- historical_dialogues/admin.py
+++ historical_dialogues/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import (
     HistoricalCharacter, CharacterTag, CharacterAchievement,
-    DialogueScenario, DialogueResponse,
-    Quiz, QuizOption,
-    UserProgress, CompletedDialogue, DiscoveredFact
+    DialogueScenario, DialogueResponse, Quiz, QuizOption,
+    UserProgress, CompletedDialogue, DiscoveredFact, ResponseKeyword, DefaultResponse
 )
 
 
