82002488eadbb0ae8835b3fa510b70147aafd989
Cherica <riantsoacherica01@gmail.com>
2025-04-06 21:54:08 +0300

    TSS DIKANY

--- GameLearnBack/settings.py
+++ GameLearnBack/settings.py
@@ -149,6 +149,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
