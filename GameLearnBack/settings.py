1fefa11dc11e5ebb0e438046ae6af65e781d3d9e
Cherica <riantsoacherica01@gmail.com>
2025-04-06 21:53:36 +0300

    TSS DIKANY

--- GameLearnBack/settings.py
+++ GameLearnBack/settings.py
@@ -56,6 +56,7 @@ AUTH_USER_MODEL = 'accounts.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
